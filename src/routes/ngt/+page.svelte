e3ec55b8fbfc77b8f237d4e67cb6991488153f78
naufalw <naufalwiwitputra@gmail.com>
2022-12-29 07:38:57 +0700

    pages: init other pages

---
+++ src/routes/ngt/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '@fontsource/titillium-web';
+</script>
+
+<div
+	class="bgblur backdrop-blur-md px-3 mx-auto sm:px-4 w-11/12 md:w-10/12 bg-red-300 text-white rounded-2xl md:rounded-3xl"
+>
+	<div class="title tracking-widest pt-2 pl-2 pb-10">NGT : NOPAL GAK TAU</div>
+</div>
+
+<style>
+	.bgblur {
+		background: rgba(22, 38, 41, 0.45);
+	}
+	.title {
+		font-family: 'Titillium Web', sans-serif;
+		font-weight: 600;
+	}
+</style>
