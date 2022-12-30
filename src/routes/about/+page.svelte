23d9573ea680365ac6586d4188659f3c67b97843
naufalw <naufalwiwitputra@gmail.com>
2022-12-30 21:52:01 +0700

    about: increase readability again

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,11 +6,11 @@
 <div
 	class="bgblur backdrop-blur-md px-3 py-3 mx-auto sm:px-4 w-11/12 md:w-10/12 bg-red-300 text-white rounded-xl md:rounded-2xl"
 >
-	<div class="title uppercase tracking-wider md:tracking-widest pl-2 text-iron-300 pb-5 md:pb-10">
+	<div class="title uppercase tracking-wider md:tracking-widest pl-5 text-iron-300 pb-5 md:pb-10">
 		About Me
 	</div>
 
-	<div class="flex flex-row  items-center w-11/12 justify-items-start ustify pl-2 ">
+	<div class="flex flex-row  items-center w-11/12 justify-items-start ustify pl-7 md:pl-8 ">
 		<div><img class="w-[30vh] md:w-[40vh]" src={naufal} alt="foto nopal gemink" /></div>
 
 		<div class="nama  flex-col  ml-4 md:ml-8   items-center">
@@ -19,13 +19,13 @@
 			>
 				Naufal Wiwit Putra
 			</div>
-			<div class="text-[14px] font-light pt-3 md:text-xl text-iron-200/[.81]">
+			<div class="text-[15px] font-light pt-3 md:text-xl text-iron-200/[.81]">
 				a 17 y.o. tech geek that loves economics and aerospace
 			</div>
 		</div>
 	</div>
-	<div class="ml-5 mt-5 mb-4 ">
-		<ul class="list-inside list-disc  text-md md:text-lg">
+	<div class="ml-8 md:ml-11 mt-5 mb-4 ">
+		<ul class="list-inside list-disc  text-[14px] md:text-lg">
 			<li class="li_item text-iron-200">Was born in 2005</li>
 			<li class="li_item text-iron-200">
 				I'm currently majoring social science in MAN 2 Kota Malang
@@ -40,7 +40,7 @@
 						Economics, mainly public policy and stock market investment
 					</li>
 					<li class="li_item text-iron-200">
-						Aerospace, all about plane’s system and rocket propulsion and aerodynamics
+						Aerospace, all about plane`s system and rocket propulsion and aerodynamics
 					</li>
 				</ul>
 			</li>
