42a74c1fd7853cb9f2101ff94423241d3c221601
naufalw <naufalwiwitputra@gmail.com>
2022-12-31 22:31:39 +0700

    title: define as new component

---
+++ src/lib/components/PageTitle.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<div class="title uppercase tracking-wider md:tracking-widest pl-2 text-iron-300 pb-7 md:pb-10">
+	{title}
+</div>
+
+<style>
+	.title {
+		font-family: 'Titillium Web', sans-serif;
+		font-weight: 400;
+	}
+</style>
