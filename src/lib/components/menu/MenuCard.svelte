9711f3520c6d4e548d12233aeb2cd65c6803c2de
naufalw <naufalwiwitputra@gmail.com>
2023-01-01 19:55:00 +0700

    menu: reduce blur again

--- src/lib/components/menu/MenuCard.svelte
+++ src/lib/components/menu/MenuCard.svelte
@@ -1,11 +1,11 @@
-<script>
-	export let title;
-	export let href;
+<script lang="ts">
+	export let title: string;
+	export let href: string;
 </script>
 
 <a {href}
 	><div
-		class="menu pt-4 md:pt-9 border border-gray-800/[.2] bg-gray-600/[.13] md:max-w-[24rem] hover:bg-gray-600/[.5] backdrop-blur rounded-br-[17px] rounded-bl-[17px] rounded-xl"
+		class="menu pt-4 md:pt-9 border border-gray-800/[.2] bg-[#16262959] backdrop-blur-sm  md:max-w-[24rem] hover:bg-[#111D20]  rounded-br-[17px] rounded-bl-[17px] rounded-xl"
 	>
 		<div class="md:pb-3.5"><slot><!-- optional fallback --></slot></div>
 
