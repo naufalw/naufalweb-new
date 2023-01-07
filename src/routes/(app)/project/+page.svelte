1d7abd5ee507ea5039065a047999566db7d135b0
naufalw <naufalwiwitputra@gmail.com>
2023-01-07 12:05:10 +0700

    project: use title component

--- src/routes/(app)/project/+page.svelte
+++ src/routes/(app)/project/+page.svelte
@@ -1,19 +1,15 @@
 <script>
 	export const prerender = true;
+	import Title from '$lib/components/PageTitle.svelte';
 </script>
 
 <svelte:head>
 	<title>Projects | Naufalw.</title>
 </svelte:head>
 
-<div class="title uppercase tracking-wider md:tracking-widest pl-2 text-iron-300 pb-7 md:pb-10">
-	projects
-</div>
-<div class="text-center text-2xl">ALSOOOO not ready yet :|</div>
+<Title title="Projects" />
+
+<div><h1>Projects</h1></div>
 
 <style>
-	.title {
-		font-family: 'Titillium Web', sans-serif;
-		font-weight: 400;
-	}
 </style>
