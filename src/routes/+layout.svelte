ad5c9f341ac8c481a9750de9d228e77b540b7f3b
naufalw <naufalwiwitputra@gmail.com>
2023-01-07 12:53:20 +0700

    nprogres: move initializer to root +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,25 @@
 	import '@fontsource/overpass/300.css';
 	import '@fontsource/overpass/400.css';
 	import { Toaster } from 'svelte-french-toast';
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	// NProgress css
+	import '$lib/nprogress.css';
+
+	NProgress.configure({
+		// Full list: https://github.com/rstacruz/nprogress#configuration
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <Toaster />
