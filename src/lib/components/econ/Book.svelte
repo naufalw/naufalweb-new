13fff887032dbf56314e411d1f6aee2fd2c5e569
naufalw <naufalwiwitputra@gmail.com>
2023-01-25 10:45:45 +0700

    econ: init book component

---
+++ src/lib/components/econ/Book.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let link: string;
+	export let title: string;
+	export let imgSource: string;
+</script>
+
+<div class=" ">
+	<a target="_blank" rel="noopener noreferrer" href={link}
+		><img
+			class=" object-center mx-auto hover:ring-4 ring-red-500 rounded-lg h-full w-full object-cover"
+			src={imgSource}
+			alt={title}
+			height="200"
+			width="100"
+		/></a
+	>
+</div>
